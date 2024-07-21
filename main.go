dcaad727a471f647872d3f819340f9309b13fa35
Christopher V <ChrisRVill@gmail.com>
2024-07-21 15:17:01 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Static("/public"))
+	e.Renderer = newTemplate()
+
+	e.GET("/", gameSelect)
+	e.GET("/Phasmo", phasmoPage)
+	e.GET("/Terraria", terrPage)
+
+	e.Logger.Fatal(e.Start(":4242"))
+}
+
+func gameSelect(c echo.Context) error {
+	return c.Render(http.StatusOK, "index", nil)
+}
+
+func phasmoPage(c echo.Context) error {
+	return c.Render(http.StatusOK, "phasmo", nil)
+}
+
+func terrPage(c echo.Context) error {
+	return c.Render(http.StatusOK, "terr", nil)
+}
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Template {
+	return &Template{
+		templates: template.Must(template.ParseGlob("public/views/html/*.html")),
+	}
+}
