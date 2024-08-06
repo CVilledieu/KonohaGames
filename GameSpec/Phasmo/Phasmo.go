3529d82d849a697f13a7694f9873fd710697b66f
Christopher V <ChrisRVill@gmail.com>
2024-08-05 23:21:44 -0700

    small updates. Still figuring out the Phasmo API

--- GameSpec/Phasmo/Phasmo.go
+++ GameSpec/Phasmo/Phasmo.go
@@ -1,74 +1,39 @@
 package Phasmo
 
-import (
-	"encoding/json"
-	"fmt"
-	"os"
-)
-
 const (
 	GHOSTCOUNT = 24
 )
 
-type _Evidence struct {
-	Name string
-}
-
-type _Ghost struct {
-	Id       string
-	Name     string
-	Evidence []string
-	Tips     []string
-}
-
-type BaseInfo struct {
-	Evidences []_Evidence
-	Ghosts    []_Ghost
-}
-
-func GetAllEvidence() []_Evidence {
-	names := []string{"Spirit Box", "Freezing Temps", "Ultraviolet", "Ghost Orbs", "EMF", "Ghost Writing", "D.O.T.S."}
-	AllEvidence := make([]_Evidence, len(names))
-	for i, n := range names {
-
-		AllEvidence[i] = _Evidence{Name: n}
-	}
-	return AllEvidence
-}
-
-func GetAllGhosts() []_Ghost {
-	AllGhost := make([]_Ghost, GHOSTCOUNT)
-	JReader, err := os.ReadFile("public/json/ghosts.json")
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	err = json.Unmarshal(JReader, &AllGhost)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	return AllGhost
-}
-
-func GetBaseInfo() BaseInfo {
-	return BaseInfo{Evidences: GetAllEvidence(), Ghosts: GetAllGhosts()}
-}
-
-// State indicates if its 1 Neither ruled out or found, 2 Found, or 3 Ruled out
 type Evidence struct {
+	Id    string
 	Name  string
 	Ghost []Ghost
-	State int
 }
 
+// Each Ghost has 3 Evidences that are a unique set. No 2 ghosts have the same set of Evidence
+// Tip is extra info about the behaviour of the ghost
 type Ghost struct {
+	Id       string
 	Name     string
-	State    int
 	Evidence []Evidence
+	Tip      []string
 }
 
 type Investigation struct {
-	Evidence []_Evidence
-	Ghosts   []_Ghost
+	Evidence []Evidence
+	Ghosts   []Ghost
+}
+
+// Used if reset button is pressed or a new page is loaded.
+// Sets all ghosts/evidence as possible and clears all ruled out evidence/ghosts
+func StartJob() Investigation {
+	return Investigation{}
+}
+
+func (inv *Investigation) RuleOutEvidence(evid Evidence) {
+
+}
+
+func (inv *Investigation) SetPossibleGhosts() {
+
 }
---
+++ public/Database/db.go
@@ -0,0 +1,7 @@
+package db
+
+import (
+	"database/sql"
+)
+
+var pool *sql.DB
